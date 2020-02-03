310fc5f64f106e8f7c660a7fa856b3ca15023ee9
lacort <lacort2@gmail.com>
2020-02-03 21:12:37 -0200

    Update

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -5,100 +5,109 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\User;
 use App\Role;
-use Gate;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
-class UserController extends Controller {
+
+class UserController extends Controller
+{
     /**
-    * Display a listing of the resource.
-    *
-    * @return \Illuminate\Http\Response
-    */
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
 
-    public function __construct() {
-        $this->middleware( 'auth' );
+    public function __construct()
+    {
+        $this->middleware('auth');
     }
 
-    public function index() {
+    public function index()
+    {
         $users = User::all();
-        return view( 'admin.users.index' )->with( 'users', $users );
+        return view('admin.users.index')->with('users', $users);
     }
 
-    public function create() {
+    public function create()
+    {
 
-        if ( Gate::denies( 'register-users' ) ) {
-            return redirect( route( 'home' ) );
+        if (Gate::denies('register-users')) {
+            return redirect(route('home'));
         }
         $roles = Role::all();
         $users = User::all();
-        return view( 'admin.users.create' )->with( [
-            'roles' =>$roles,
+        return view('admin.users.create')->with([
+            'roles' => $roles,
             'user' => $users
-        ] );
+        ]);
     }
 
-    public function register( Request $request ) {
+    public function register(Request $request)
+    {
 
         $user = new user();
 
         $user->name = $request->name;
         $user->email = $request->email;
-        $user->password = Hash::make( $request->password );
+        $user->password = Hash::make($request->password);
         //$user->roles = $request->role;
         $user->save();
 
-        return redirect()->route( 'admin.users.index' );
+        return redirect()->route('admin.users.index');
     }
 
     /**
-    * Show the form for editing the specified resource.
-    *
-    * @param  \App\User  $user
-    * @return \Illuminate\Http\Response
-    */
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
 
-    public function edit( User $user ) {
-        if ( Gate::denies( 'edit-users' ) ) {
-            return redirect( route( 'admin.users.index' ) );
+    public function edit(User $user)
+    {
+        if (Gate::denies('edit-users')) {
+            return redirect(route('admin.users.index'));
         }
 
         $roles = Role::all();
 
-        return view( 'admin.users.edit' )->with( [
+        return view('admin.users.edit')->with([
             'user' => $user,
-            'roles' =>$roles
-        ] );
+            'roles' => $roles
+        ]);
     }
 
     /**
-    * Update the specified resource in storage.
-    *
-    * @param  \Illuminate\Http\Request  $request
-    * @param  \App\User  $user
-    * @return \Illuminate\Http\Response
-    */
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
 
-    public function update( Request $request, User $user ) {
-        $user->roles()->sync( $request->roles );
-        return redirect()->route( 'admin.users.index' );
+    public function update(Request $request, User $user)
+    {
+        $user->roles()->sync($request->roles);
+        return redirect()->route('admin.users.index');
     }
 
     /**
-    * Remove the specified resource from storage.
-    *
-    * @param  \App\User  $user
-    * @return \Illuminate\Http\Response
-    */
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
 
-    public function destroy( User $user ) {
+    public function destroy(User $user)
+    {
 
-        if ( Gate::denies( 'delete-users' ) ) {
-            return redirect( route( 'admin.users.index' ) );
+        if (Gate::denies('delete-users')) {
+            return redirect(route('admin.users.index'));
         }
         $user->roles()->detach();
         $user->delete();
 
-        return redirect()->route( 'admin.users.index' );
+        return redirect()->route('admin.users.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
-
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is where you can register web routes for your application. Thesei
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
@@ -17,11 +18,11 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');     
+Route::get('/home', 'HomeController@index')->name('home');
 
-Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function(){
-    Route::resource('/users', 'UserController', ['except'=>['show','create','store']]);
-    Route::get('/users/create','UserController@create')->name('create');
-    
+Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function () {
+    Route::resource('/users', 'UserController', ['except' => ['show', 'create', 'store']]);
+    Route::get('/users/create', 'UserController@create')->name('create');
+
     Route::post('/users/add', 'UserController@register')->name('register');
 });
